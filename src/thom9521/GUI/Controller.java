112bb86e5b101c06953ae73661553db460d36b6c
Thom9521 <Thom9521@edu.easj.dk>
2018-03-05 18:37:17 +0100

    Tilføjelser til GUI

--- src/thom9521/GUI/Controller.java
+++ src/thom9521/GUI/Controller.java
@@ -1,4 +1,12 @@
 package thom9521.GUI;
 
+import javax.swing.*;
+
 public class Controller {
+
+
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JButton loginButton;
+
 }
