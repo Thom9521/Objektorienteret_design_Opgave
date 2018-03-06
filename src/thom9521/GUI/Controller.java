d73b74999f9efa2e0851edde8c313ed0a3f23349
Thom9521 <Thom9521@edu.easj.dk>
2018-03-06 19:18:02 +0100

    Tilføjelser til GUI

--- src/thom9521/GUI/Controller.java
+++ src/thom9521/GUI/Controller.java
@@ -1,12 +1,50 @@
 package thom9521.GUI;
 
-import javax.swing.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;;
+
+
 
 public class Controller {
 
+    @FXML
+    private TextField telefonField;
+    @FXML
+    private PasswordField passwordField;
+    @FXML
+    private Button loginButton;
+    @FXML
+    private Label loginText;
+/*
+    public boolean checkLogin(){
+        boolean check = false;
+        if(telfonField.getText() == "30306406"){
+            if (passwordField.getText() == "test") {
+                check = true;
+            }
+                else{
+                    check = false;}
 
-    private JTextField usernameField;
-    private JPasswordField passwordField;
-    private JButton loginButton;
+                } return check;
 
+            }
+*/
+
+
+
+    @FXML
+    private void handleButtonAction(ActionEvent e) {
+
+    if (e.getSource()==loginButton){
+        if (telefonField.getText()== "Hej"){
+            loginText.setText("Sådan");
+        }
+
+    }
+
+    }
 }
